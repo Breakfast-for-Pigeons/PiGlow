8169ab3f3064efb46a927c504ae686de97839d2d
Paul Ryan <ShineTop@users.noreply.github.com>
2015-08-02 14:28:58 -0500

    Update and rename Inward_Out_3.py to Inside_Out_3.py

--- Programs/Inside_Out_3.py
+++ Programs/Inside_Out_3.py
@@ -1,5 +1,5 @@
 ################################################################
-#                        Inward Out 3                          #
+#                        Inside Out 3                          #
 ################################################################
 # Description:                                                 #
 # A variation of Inward Out. This program lights up the        #
