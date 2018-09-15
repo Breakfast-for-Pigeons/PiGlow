bbac4c0e41785f7bcef638e1a9e5d814d851cb68
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-15 10:14:52 -0500

    Update one_through_eighteen.py

--- Programs/one_through_eighteen.py
+++ Programs/one_through_eighteen.py
@@ -44,12 +44,35 @@ def main():
     """
     The main function
     """
-
+    print("Press Ctrl-C to stop the program.")
     try:
         increase_speed()
     # Stop the program and turn off LEDs with Ctrl-C
     except KeyboardInterrupt:
+        print("\nExiting program.")
         PYGLOW.all(0)
+
+
+def increase_speed():
+    """
+    Gradually increases the speed at which the LEDs light up
+    """
+    # Uncomment the following line for testing/debugging
+    # print("Increasing speed...")
+    led_number = 1
+    sleep_speed = 0.25
+    while sleep_speed > 0.05:
+        # Uncomment the following line for testing/debugging
+        # print("The speed is now: ", sleep_speed)
+        while led_number < 19:
+            PYGLOW.led(led_number, 100)    # light up LED
+            sleep(sleep_speed)
+            PYGLOW.led(led_number, 0)      # turn off LED
+            sleep(sleep_speed)
+            led_number += 1
+        led_number = 1                     # Reset LED number to 1
+        sleep_speed -= 0.05                # Increase speed
+    run_10_times()
 
 
 def run_10_times():
@@ -75,28 +98,6 @@ def run_10_times():
             led_number += 1                # increment LED number
         counter -= 1                       # decrease counter
     increase_speed()
-
-
-def increase_speed():
-    """
-    Gradually increases the speed at which the LEDs light up
-    """
-    # Uncomment the following line for testing/debugging
-    # print("Increasing speed...")
-    led_number = 1
-    sleep_speed = 0.25
-    while sleep_speed > 0.05:
-        # Uncomment the following line for testing/debugging
-        # print("The speed is now: ", sleep_speed)
-        while led_number < 19:
-            PYGLOW.led(led_number, 100)    # light up LED
-            sleep(sleep_speed)
-            PYGLOW.led(led_number, 0)      # turn off LED
-            sleep(sleep_speed)
-            led_number += 1
-        led_number = 1                     # Reset LED number to 1
-        sleep_speed -= 0.05                # Increase speed
-    run_10_times()
 
 
 if __name__ == '__main__':
