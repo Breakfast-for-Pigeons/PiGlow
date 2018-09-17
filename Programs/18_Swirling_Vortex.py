13e6798e67597af0c07c2b9082ce648c8e5bef08
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-17 12:13:31 -0500

    Update 18_Swirling_Vortex.py

--- Programs/18_Swirling_Vortex.py
+++ Programs/18_Swirling_Vortex.py
@@ -1,136 +1,165 @@
-################################################################
-#                     Swirling Vortex                          #
-################################################################
-# Description:                                                 #
-# This program lights up the LEDs on one at at time then       #
-# then fades them on all 3 arms at the same time.              #
-#                                                              #
-# Requirements: PyGlow.py                                      #
-#                                                              #
-# Author: Paul Ryan                                            #
-#                                                              #
-################################################################
+#!/usr/bin/python3
+"""
+Swirling Vortex
 
-from PyGlow import PyGlow
+This program lights up the LEDs one at at time then
+then fades them on all 3 arms at the same time.
+
+....................
+
+Functions:
+- swirling_vortex: Lights up 1 color at a time, Fades colors
+
+....................
+
+Requirements: PyGlow.py
+
+....................
+
+Author: Paul Ryan
+
+This program was written on a Raspberry Pi using the Geany IDE.
+"""
+########################################################################
+#                          Import modules                              #
+########################################################################
+
 from time import sleep
+from PyGlow import PyGlow
 
-pyglow = PyGlow()
+########################################################################
+#                           Variables                                  #
+########################################################################
 
-# Initialize
-pyglow.all(0)
+PYGLOW = PyGlow()
+SLEEP_SPEED = 0.01
 
-# Variables
-sleep_speed = 0.01
+########################################################################
+#                            Functions                                 #
+########################################################################
 
-# Functions
+
+def main():
+    """
+    The main function
+    """
+    print("Press Ctrl-C to stop the program.")
+    try:
+        while True:
+            swirling_vortex()
+            sleep(1)
+    # Stop the program and turn off LEDs with Ctrl-C
+    except KeyboardInterrupt:
+        print("\nExiting program.")
+        PYGLOW.all(0)
+
+
 def swirling_vortex():
+    """
+    Lights up 1 color at a time, Fades colors
+    """
     # Turn on red
-    pyglow.color("red", 60)
-    sleep(sleep_speed)
+    PYGLOW.color("red", 60)
+    sleep(SLEEP_SPEED)
     # Turn on orange
-    pyglow.color("orange", 60)
-    sleep(sleep_speed)
+    PYGLOW.color("orange", 60)
+    sleep(SLEEP_SPEED)
     # Fade red
-    pyglow.color("red", 50)
-    sleep(sleep_speed)
+    PYGLOW.color("red", 50)
+    sleep(SLEEP_SPEED)
     # Turn on yellow
-    pyglow.color("yellow", 60)
-    sleep(sleep_speed)
+    PYGLOW.color("yellow", 60)
+    sleep(SLEEP_SPEED)
     # Fade red and orange
-    pyglow.color("red", 40)
-    sleep(sleep_speed)
-    pyglow.color("orange", 50)
-    sleep(sleep_speed)
+    PYGLOW.color("red", 40)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("orange", 50)
+    sleep(SLEEP_SPEED)
     # Turn on green
-    pyglow.color("green", 60)
-    sleep(sleep_speed)
+    PYGLOW.color("green", 60)
+    sleep(SLEEP_SPEED)
     # Fade red, orange, and yellow
-    pyglow.color("red", 30)
-    sleep(sleep_speed)
-    pyglow.color("orange", 40)
-    sleep(sleep_speed)
-    pyglow.color("yellow", 50)
-    sleep(sleep_speed)
+    PYGLOW.color("red", 30)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("orange", 40)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("yellow", 50)
+    sleep(SLEEP_SPEED)
     # Turn on blue
-    pyglow.color("blue", 60)
-    sleep(sleep_speed)
+    PYGLOW.color("blue", 60)
+    sleep(SLEEP_SPEED)
     # Fade red, orange, yellow, green
-    pyglow.color("red", 20)
-    sleep(sleep_speed)
-    pyglow.color("orange", 30)
-    sleep(sleep_speed)
-    pyglow.color("yellow", 40)
-    sleep(sleep_speed)
-    pyglow.color("green", 50)
-    sleep(sleep_speed)
+    PYGLOW.color("red", 20)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("orange", 30)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("yellow", 40)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("green", 50)
+    sleep(SLEEP_SPEED)
     # Turn on white
-    pyglow.color("white", 60)
-    sleep(sleep_speed)
+    PYGLOW.color("white", 60)
+    sleep(SLEEP_SPEED)
     # Fade red, orange, yellow, green, and blue
-    pyglow.color("red", 10)
-    sleep(sleep_speed)
-    pyglow.color("orange", 20)
-    sleep(sleep_speed)
-    pyglow.color("yellow", 30)
-    sleep(sleep_speed)
-    pyglow.color("green", 40)
-    sleep(sleep_speed)
-    pyglow.color("blue", 50)
-    sleep(sleep_speed)
+    PYGLOW.color("red", 10)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("orange", 20)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("yellow", 30)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("green", 40)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("blue", 50)
+    sleep(SLEEP_SPEED)
     # Fade all
-    pyglow.color("red", 0)
-    sleep(sleep_speed)
-    pyglow.color("orange", 10)
-    sleep(sleep_speed)
-    pyglow.color("yellow", 20)
-    sleep(sleep_speed)
-    pyglow.color("green", 30)
-    sleep(sleep_speed)
-    pyglow.color("blue", 40)
-    sleep(sleep_speed)
-    pyglow.color("white", 50)
-    sleep(sleep_speed)
+    PYGLOW.color("red", 0)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("orange", 10)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("yellow", 20)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("green", 30)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("blue", 40)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("white", 50)
+    sleep(SLEEP_SPEED)
     # Fade orange, yellow, green, blue, and white
-    pyglow.color("orange", 0)
-    sleep(sleep_speed)
-    pyglow.color("yellow", 10)
-    sleep(sleep_speed)
-    pyglow.color("green", 20)
-    sleep(sleep_speed)
-    pyglow.color("blue", 30)
-    sleep(sleep_speed)
-    pyglow.color("white", 40)
-    sleep(sleep_speed)
+    PYGLOW.color("orange", 0)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("yellow", 10)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("green", 20)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("blue", 30)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("white", 40)
+    sleep(SLEEP_SPEED)
     # Fade yellow, green, blue, and white
-    pyglow.color("yellow", 0)
-    sleep(sleep_speed)
-    pyglow.color("green", 10)
-    sleep(sleep_speed)
-    pyglow.color("blue", 20)
-    sleep(sleep_speed)
-    pyglow.color("white", 30)
-    sleep(sleep_speed)
+    PYGLOW.color("yellow", 0)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("green", 10)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("blue", 20)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("white", 30)
+    sleep(SLEEP_SPEED)
     # Fade green, blue, and white
-    pyglow.color("green", 0)
-    sleep(sleep_speed)
-    pyglow.color("blue", 10)
-    sleep(sleep_speed)
-    pyglow.color("white", 20)
-    sleep(sleep_speed)
+    PYGLOW.color("green", 0)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("blue", 10)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("white", 20)
+    sleep(SLEEP_SPEED)
     # Fade blue, and white
-    pyglow.color("blue", 0)
-    sleep(sleep_speed)
-    pyglow.color("white", 10)
-    sleep(sleep_speed)
+    PYGLOW.color("blue", 0)
+    sleep(SLEEP_SPEED)
+    PYGLOW.color("white", 10)
+    sleep(SLEEP_SPEED)
     # Fade white
-    pyglow.color("white", 0)
-    sleep(sleep_speed)
+    PYGLOW.color("white", 0)
+    sleep(SLEEP_SPEED)
 
-# Start the program
-try:   
-    while True:
-        swirling_vortex()
-        sleep(1)
-# Stop the program and turn off LEDs with Ctrl-C
-except KeyboardInterrupt:
-    pyglow.all()
+
+if __name__ == '__main__':
+    main()
