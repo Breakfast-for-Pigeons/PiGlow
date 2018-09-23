123f8d718a23e2c35bad43b757f5eac05e073db9
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-23 00:26:06 -0500

    Update fading_spiral_arms.py

--- Programs/fading_spiral_arms.py
+++ Programs/fading_spiral_arms.py
@@ -11,6 +11,9 @@ gradually fade.
 
 Functions:
 - fading_sprial_arms: fades the spiral arms
+- run_10_times: Cycles throught the LEDs 10 times
+- delete_empty_logs: Deletes empty log fles
+- stop: Print exit message and turn off the PiGlow
 
 ....................
 
@@ -22,14 +25,15 @@ Author: Paul Ryan
 
 This program was written on a Raspberry Pi using the Geany IDE.
 """
-
-
 ########################################################################
 #                          Import modules                              #
 ########################################################################
 
+import os
+import logging
 from time import sleep
 from PyGlow import PyGlow
+from print_piglow_header import print_piglow_header
 
 ########################################################################
 #                           Initialize                                 #
@@ -37,6 +41,17 @@ from PyGlow import PyGlow
 
 PYGLOW = PyGlow()
 PYGLOW.all(0)
+
+# Logging
+LOG = 'fading_spiral_arms.log'
+LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: %(levelname)s: %(message)s'
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.ERROR)    # Nothing will log unless changed to DEBUG
+FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
+                              datefmt='%m/%d/%y %I:%M:%S %p:')
+FILE_HANDLER = logging.FileHandler(LOG, 'w')
+FILE_HANDLER.setFormatter(FORMATTER)
+LOGGER.addHandler(FILE_HANDLER)
 
 ########################################################################
 #                            Functions                                 #
@@ -47,20 +62,26 @@ def main():
     """
     The main function
     """
-    print("Press Ctrl-C to stop the program.")
+    LOGGER.debug("START")
+
+    print_piglow_header()
+
+    # Force white text after selecting random colored header
+    print("\033[1;37;40mPress Ctrl-C to stop the program.")
     try:
-        while True:
-            fading_spiral_arms()
-            sleep(0.5)
+        run_10_times()
+        stop()
     # Stop the program and turn off LEDs with Ctrl-C
     except KeyboardInterrupt:
-        print("\nExiting program.")
-        PYGLOW.all(0)
+        stop()
 
 
 def fading_spiral_arms():
     """ Fades the spiral arms """
+    LOGGER.debug("Fading spiral arms...")
+
     sleep_speed = 0.01
+
     # Turn LED 1
     PYGLOW.led(1, 180)
     sleep(sleep_speed)
@@ -798,6 +819,51 @@ def fading_spiral_arms():
     # Fade LED 18
     PYGLOW.led(18, 0)
     sleep(sleep_speed)
+
+
+def run_10_times():
+    """ Run 10 times
+
+    Once the program was reached the max set speed, it will cycle
+    throught the LEDs 10 times.
+
+    """
+    LOGGER.debug("Running 10 times...")
+
+    counter = 10
+
+    while counter > 0:
+        LOGGER.debug("Running number: %s", counter)
+        fading_spiral_arms()
+        # Decrease counter
+        counter -= 1
+
+
+def delete_empty_logs():
+    """
+    Delete empty log fles
+
+    Log files will always be created. But they will be empty if the
+    log level is set to anything higher than DEBUG, since only DEBUG
+    messages are logged. If the log files are empty, they will be
+    deleted.
+    """
+
+    logs = [LOG, 'print_piglow_header.log']
+
+    for log in logs:
+        if os.stat(log).st_size == 0:
+            os.remove(log)
+
+
+def stop():
+    """
+    Print exit message and turn off the PiGlow
+    """
+    LOGGER.debug("END")
+    delete_empty_logs()
+    print("\nExiting program.")
+    PYGLOW.all(0)
 
 
 if __name__ == '__main__':
