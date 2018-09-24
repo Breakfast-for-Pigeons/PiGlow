cac205eef63a8146cedd1b78ca36a68123e9007f
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-24 10:36:35 -0500

    Update snakes.py

--- Programs/snakes.py
+++ Programs/snakes.py
@@ -10,10 +10,16 @@ Functions:
 - snake_12: Lights up the LEDs on arms 1 and 2
 - snake_13: Lights up the LEDs on arms 1 and 3
 - snake_23: Lights up the LEDs on arms 2 and 3
+- delete_empty_logs: Deletes empty log fles
+- stop: Print exit message and turn off the PiGlow
 
 ....................
 
-Requirements: PyGlow.py
+Requirements:
+    PyGlow.py (many thanks to benleb for this program)
+    print_piglow_header.py
+
+You will have these files if you downloaded the entire repository.
 
 ....................
 
@@ -25,29 +31,29 @@ This program was written on a Raspberry Pi using the Geany IDE.
 #                          Import modules                              #
 ########################################################################
 
+import os
+import logging
 from time import sleep
 from PyGlow import PyGlow
+from print_piglow_header import print_piglow_header
 
 ########################################################################
-#                           Variables                                  #
+#                           Initialize                                 #
 ########################################################################
 
 PYGLOW = PyGlow()
-SLEEP_SPEED = 2
+PYGLOW.all(0)
 
-########################################################################
-#                            Lists                                     #
-########################################################################
-
-# Snake 12 LEDs (Same lights as Snake 21 - Since they are not slithering,
-# the order doesn't matter)
-SNAKE_12_LEDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 18]
-# Snake 13 LEDs (Same lights as Snake 31 - Since they are not slithering,
-# the order doesn't matter)
-SNAKE_13_LEDS = [1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 18]
-# Snake 23 LEDs (Same lights as Snake 32 - Since they are not slithering,
-# the order doesn't matter)
-SNAKE_23_LEDS = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+# Logging
+LOG = 'snakes.log'
+LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: %(levelname)s: %(message)s'
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.ERROR)    # Nothing will log unless changed to DEBUG
+FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
+                              datefmt='%m/%d/%y %I:%M:%S %p:')
+FILE_HANDLER = logging.FileHandler(LOG, 'w')
+FILE_HANDLER.setFormatter(FORMATTER)
+LOGGER.addHandler(FILE_HANDLER)
 
 ########################################################################
 #                            Functions                                 #
@@ -58,52 +64,109 @@ def main():
     """
     The main function
     """
-    print("Press Ctrl-C to stop the program.")
+    LOGGER.debug("START")
+
+    print_piglow_header()
+
+    # Force white text after selecting random colored header
+    print("\033[1;37;40mPress Ctrl-C to stop the program.")
+
+    counter = 1
+
     try:
-        while True:
+        while counter < 4:
+            LOGGER.debug("counter = %s", counter)
             snake_12()
             snake_23()
             snake_13()
+            counter += 1
+        stop()
     # Stop the program and turn off LEDs with Ctrl-C
     except KeyboardInterrupt:
-        print("\nExiting program.")
-        PYGLOW.all(0)
-
+        stop()
 
 def snake_12():
     """
     Lights up the LEDs on arms 1 and 2
     """
-    PYGLOW.set_leds(SNAKE_12_LEDS, 100)
+    LOGGER.debug("Lighting up Snake 1-2")
+
+    # Snake 12 LEDs (Same lights as Snake 21 - Since they are not
+    # slithering, the order doesn't matter)
+    snake_12_leds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 18]
+    sleep_speed = 2
+
+    PYGLOW.set_leds(snake_12_leds, 100)
     PYGLOW.update_leds()
-    sleep(SLEEP_SPEED)
-    PYGLOW.set_leds(SNAKE_12_LEDS, 0)
+    sleep(sleep_speed)
+    PYGLOW.set_leds(snake_12_leds, 0)
     PYGLOW.update_leds()
-    sleep(SLEEP_SPEED)
+    sleep(sleep_speed)
 
 
 def snake_13():
     """
     Lights up the LEDs on arms 1 and 3
     """
-    PYGLOW.set_leds(SNAKE_13_LEDS, 100)
+    LOGGER.debug("Lighting up Snake 1-3")
+
+    # Snake 13 LEDs (Same lights as Snake 31 - Since they are not
+    # slithering, the order doesn't matter)
+    snake_13_leds = [1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 18]
+    sleep_speed = 2
+
+    PYGLOW.set_leds(snake_13_leds, 100)
     PYGLOW.update_leds()
-    sleep(SLEEP_SPEED)
-    PYGLOW.set_leds(SNAKE_13_LEDS, 0)
+    sleep(sleep_speed)
+    PYGLOW.set_leds(snake_13_leds, 0)
     PYGLOW.update_leds()
-    sleep(SLEEP_SPEED)
+    sleep(sleep_speed)
 
 
 def snake_23():
     """
     Lights up the LEDs on arms 2 and 3
     """
-    PYGLOW.set_leds(SNAKE_23_LEDS, 100)
+    LOGGER.debug("Lighting up Snake 2-3")
+
+    # Snake 23 LEDs (Same lights as Snake 32 - Since they are not
+    # slithering, the order doesn't matter)
+    snake_23_leds = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    sleep_speed = 2
+
+    PYGLOW.set_leds(snake_23_leds, 100)
     PYGLOW.update_leds()
-    sleep(SLEEP_SPEED)
-    PYGLOW.set_leds(SNAKE_23_LEDS, 0)
+    sleep(sleep_speed)
+    PYGLOW.set_leds(snake_23_leds, 0)
     PYGLOW.update_leds()
-    sleep(SLEEP_SPEED)
+    sleep(sleep_speed)
+
+
+def delete_empty_logs():
+    """
+    Delete empty log fles
+
+    Log files will always be created. But they will be empty if the
+    log level is set to anything higher than DEBUG, since only DEBUG
+    messages are logged. If the log files are empty, they will be
+    deleted.
+    """
+
+    logs = [LOG, 'print_piglow_header.log']
+
+    for log in logs:
+        if os.stat(log).st_size == 0:
+            os.remove(log)
+
+
+def stop():
+    """
+    Print exit message and turn off the PiGlow
+    """
+    LOGGER.debug("END")
+    delete_empty_logs()
+    print("\nExiting program.")
+    PYGLOW.all(0)
 
 
 if __name__ == '__main__':
