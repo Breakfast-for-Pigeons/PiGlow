65a0a83b23280642a9aa9a01a0b61485c1ee1cba
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-25 13:42:46 -0500

    Update spinning_fan.py

--- Programs/spinning_fan.py
+++ Programs/spinning_fan.py
@@ -14,10 +14,16 @@ Functions:
 - go_fast: Sleep_speed goes from 0.05 to 0.01 in decrements of 0.0025
 - go_faster: Sleep_speed  is 0.01. Cycle through the LEDS 20 times
 - go_really_fast: Sleep_speed is 0. Cycle through the LEDS 100 times
+- delete_empty_logs: Deletes empty log fles
+- stop: Print exit message and turn off the PiGlow
 
 ....................
 
-Requirements: PyGlow.py
+Requirements:
+    PyGlow.py (many thanks to benleb for this program)
+    print_piglow_header.py
+
+You will have these files if you downloaded the entire repository.
 
 ....................
 
@@ -29,20 +35,29 @@ This program was written on a Raspberry Pi using the Geany IDE.
 #                          Import modules                              #
 ########################################################################
 
+import os
+import logging
 from time import sleep
 from PyGlow import PyGlow
-
-########################################################################
-#                           Variables                                  #
-########################################################################
-
-PYGLOW = PyGlow()
+from print_piglow_header import print_piglow_header
 
 ########################################################################
 #                           Initialize                                 #
 ########################################################################
 
+PYGLOW = PyGlow()
 PYGLOW.all(0)
+
+# Logging
+LOG = 'spinning_fan.log'
+LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: %(levelname)s: %(message)s'
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.ERROR)    # Nothing will log unless changed to DEBUG
+FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
+                              datefmt='%m/%d/%y %I:%M:%S %p:')
+FILE_HANDLER = logging.FileHandler(LOG, 'w')
+FILE_HANDLER.setFormatter(FORMATTER)
+LOGGER.addHandler(FILE_HANDLER)
 
 ########################################################################
 #                            Functions                                 #
@@ -53,31 +68,33 @@ def main():
     """
     The main function
     """
-    print("Press Ctrl-C to stop the program.")
+    LOGGER.debug("START")
+
+    print_piglow_header()
+
+    # Force white text after selecting random colored header
+    print("\033[1;37;40mPress Ctrl-C to stop the program.")
     try:
-        while True:
-            spinning_fan()
-            go_fast()
-            go_faster()
-            go_really_fast()
+        spinning_fan()
+        go_fast()
+        go_faster()
+        go_really_fast()
+        stop()
     # Stop the program and turn off LEDs with Ctrl-C
     except KeyboardInterrupt:
-        print("\nExiting program.")
-        PYGLOW.all(0)
+        stop()
 
 
 def spinning_fan():
     """
     Turns on one arm at a time.
     """
-    # Uncomment the following line for testing/debugging
-    # print("Increasing speed...")
+    LOGGER.debug("Increasing speed...")
 
     sleep_speed = 0.5
 
     while sleep_speed > 0.05:
-        # Uncomment the following line for testing/debugging
-        # print("The speed is now: ", sleep_speed)
+        LOGGER.debug("The speed is now: %s", sleep_speed)
         # Arm 1
         PYGLOW.arm(1, 100)
         sleep(sleep_speed)
@@ -98,14 +115,12 @@ def go_fast():
     """
     Sleep_speed goes from 0.05 to 0.01 in decrements of 0.0025
     """
-    # Uncomment the following line for testing/debugging
-    # print("Going fast...")
+    LOGGER.debug("Going fast...")
 
     sleep_speed = 0.05
 
     while sleep_speed > 0.01:
-        # Uncomment the following line for testing/debugging
-        # print("counter = ", counter)
+        LOGGER.debug("The speed is now: %s", sleep_speed)
         # Arm 1
         PYGLOW.arm(1, 100)
         sleep(sleep_speed)
@@ -126,15 +141,13 @@ def go_faster():
     """
     Sleep_speed is 0.01. Cycle through the LEDS 20 times
     """
-    # Uncomment the following line for testing/debugging
-    # print("Going faster...")
+    LOGGER.debug("Going faster...")
 
     sleep_speed = 0.01
     counter = 50
 
     while counter > 0:
-        # Uncomment the following line for testing/debugging
-        # print("counter = ", counter)
+        LOGGER.debug("counter = %s", counter)
         # Arm 1
         PYGLOW.arm(1, 100)
         sleep(sleep_speed)
@@ -155,15 +168,13 @@ def go_really_fast():
     """
     Sleep_speed is 0. Cycle through the LEDS 100 times
     """
-    # Uncomment the following line for testing/debugging
-    # print("Going really fast...")
+    LOGGER.debug("Going really fast...")
 
     sleep_speed = 0
     counter = 100
 
     while counter > 0:
-        # Uncomment the following line for testing/debugging
-        # print("counter = ", counter)
+        LOGGER.debug("counter = %s", counter)
         # Arm 1
         PYGLOW.arm(1, 100)
         sleep(sleep_speed)
@@ -178,7 +189,33 @@ def go_really_fast():
         PYGLOW.arm(3, 0)
         # decrease counter
         counter -= 1
-    sleep(2)
+
+
+def delete_empty_logs():
+    """
+    Delete empty log fles
+
+    Log files will always be created. But they will be empty if the
+    log level is set to anything higher than DEBUG, since only DEBUG
+    messages are logged. If the log files are empty, they will be
+    deleted.
+    """
+
+    logs = [LOG, 'print_piglow_header.log']
+
+    for log in logs:
+        if os.stat(log).st_size == 0:
+            os.remove(log)
+
+
+def stop():
+    """
+    Print exit message and turn off the PiGlow
+    """
+    LOGGER.debug("END")
+    delete_empty_logs()
+    print("\nExiting program.")
+    PYGLOW.all(0)
 
 
 if __name__ == '__main__':
