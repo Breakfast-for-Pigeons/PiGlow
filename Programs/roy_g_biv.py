9b69eb2afd44c4574bca0c8d1e554d7ef7fa0aa8
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-25 10:24:44 -0500

    Update roy_g_biv.py

--- Programs/roy_g_biv.py
+++ Programs/roy_g_biv.py
@@ -1,104 +1,204 @@
-################################################################
-#                        Roy G. Biv                            #
-################################################################
-# Description:                                                 #
-# This program lights up like colors one at the same,          #
-# all red, all orange, etc... then gradually increases the     #
-# speed and then goes through the entire process again.        # 
-#                                                              #
-# Requirements: PyGlow.py                                      #
-#                                                              #
-# Author: Paul Ryan                                            #
-#                                                              #
-################################################################
+#!/usr/bin/python3
+"""
+Roy G. Biv
 
-from PyGlow import PyGlow
+This program lights up like colors one at the same,
+all red, all orange, etc... then gradually increases the
+speed and then goes through the entire process again.
+
+....................
+
+Functions:
+
+- roy_g_biv: Controls which fuse to light
+- increase_speed: Gradually increases the speed
+- go_fast: Sleep_speed goes from 0.05 to 0.01 in decrements of 0.0025
+- go_faster: Sleep_speed  is 0.01. Cycle through the LEDS 20 times
+- go_really_fast: Sleep_speed is 0. Cycle through the LEDS 100 times
+- delete_empty_logs: Deletes empty log fles
+- stop: Print exit message and turn off the PiGlow
+
+....................
+
+Requirements:
+    PyGlow.py (many thanks to benleb for this program)
+    print_piglow_header.py
+
+You will have these files if you downloaded the entire repository.
+
+....................
+
+Author: Paul Ryan
+
+This program was written on a Raspberry Pi using the Geany IDE.
+"""
+########################################################################
+#                          Import modules                              #
+########################################################################
+
+import os
+import logging
 from time import sleep
+from PyGlow import PyGlow
+from print_piglow_header import print_piglow_header
 
-pyglow = PyGlow()
+########################################################################
+#                           Initialize                                 #
+########################################################################
 
-# Initialize
-pyglow.all(0)
+PYGLOW = PyGlow()
+PYGLOW.all(0)
 
-# Variables
-counter = 5
-sleep_speed = 0.25
+# Logging
+LOG = 'roy_g_biv.log'
+LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: %(levelname)s: %(message)s'
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.ERROR)    # Nothing will log unless changed to DEBUG
+FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
+                              datefmt='%m/%d/%y %I:%M:%S %p:')
+FILE_HANDLER = logging.FileHandler(LOG, 'w')
+FILE_HANDLER.setFormatter(FORMATTER)
+LOGGER.addHandler(FILE_HANDLER)
 
-# Functions
-def roy_g_biv(sleep_speed):
-    pyglow.color("red",100)
-    sleep(sleep_speed)
-    pyglow.color("red",0)
-    pyglow.color("orange",100)
-    sleep(sleep_speed)
-    pyglow.color("orange",0)
-    pyglow.color("yellow",100)
-    sleep(sleep_speed)
-    pyglow.color("yellow",0)
-    pyglow.color("green",100)
-    sleep(sleep_speed)
-    pyglow.color("green",0)
-    pyglow.color("blue",100)
-    sleep(sleep_speed)
-    pyglow.color("blue",0)
-    pyglow.color("white",100)
-    sleep(sleep_speed)
-    pyglow.color("white",0) 
-    
+########################################################################
+#                            Functions                                 #
+########################################################################
+
+
+def main():
+    """
+    The main function
+    """
+    LOGGER.debug("START")
+
+    print_piglow_header()
+
+    # Force white text after selecting random colored header
+    print("\033[1;37;40mPress Ctrl-C to stop the program.")
+    try:
+        increase_speed()
+        go_fast()
+        go_faster()
+        go_really_fast()
+        stop()
+    # Stop the program and turn off LEDs with Ctrl-C
+    except KeyboardInterrupt:
+        stop()
+
+
 def increase_speed():
-    ''' Uncomment the following line for feedback while the program is running '''
-    #print "Increasing speed..."
+    """
+    Gradually increases the speed at which the LEDs light up
+    """
+    LOGGER.debug("Increasing speed...")
+
     sleep_speed = 0.25
+
     while sleep_speed > 0.05:
-        ''' Uncomment the following line for feedback while the program is running '''
-        # print "The speed is now: ", sleep_speed
+        LOGGER.debug("The speed is now: %s", sleep_speed)
         roy_g_biv(sleep_speed)
         # Increase speed
         sleep_speed -= 0.05
-    go_fast(sleep_speed)
 
-def go_fast(sleep_speed):
+
+def roy_g_biv(sleep_speed):
+    """
+    Cylces through all the colors
+    """
+    PYGLOW.color("red", 100)
+    sleep(sleep_speed)
+    PYGLOW.color("red", 0)
+    PYGLOW.color("orange", 100)
+    sleep(sleep_speed)
+    PYGLOW.color("orange", 0)
+    PYGLOW.color("yellow", 100)
+    sleep(sleep_speed)
+    PYGLOW.color("yellow", 0)
+    PYGLOW.color("green", 100)
+    sleep(sleep_speed)
+    PYGLOW.color("green", 0)
+    PYGLOW.color("blue", 100)
+    sleep(sleep_speed)
+    PYGLOW.color("blue", 0)
+    PYGLOW.color("white", 100)
+    sleep(sleep_speed)
+    PYGLOW.color("white", 0)
+
+
+def go_fast():
+    """
+    Sleep_speed goes from 0.05 to 0.01 in decrements of 0.0025
+    """
+    LOGGER.debug("Going fast...")
+
     sleep_speed = 0.05
-    ''' Uncomment the following line for feedback while the program is running '''
-    #print "Going fast..."
+
     while sleep_speed > 0.01:
-        ''' Uncomment the following line for feedback while the program is running '''
-        #print "counter = ", counter
+        LOGGER.debug("The speed is now: %s", sleep_speed)
         roy_g_biv(sleep_speed)
         # decrease counter
         sleep_speed -= 0.0025
-    go_faster()
+
 
 def go_faster():
+    """
+    Sleep_speed is 0.01. Cycle through the LEDS 20 times
+    """
+    LOGGER.debug("Going faster...")
+
     sleep_speed = 0.01
-    ''' Uncomment the following line for feedback while the program is running '''
-    #print "Going faster..."
     counter = 20
+
     while counter > 0:
-        ''' Uncomment the following line for feedback while the program is running '''
-        #print "counter = ", counter
+        LOGGER.debug("counter = %s", counter)
         roy_g_biv(sleep_speed)
         # decrease counter
         counter -= 1
-    go_really_fast()
+
 
 def go_really_fast():
+    """
+    Sleep_speed is 0. Cycle through the LEDS 100 times
+    """
+    LOGGER.debug("Going really fast...")
+
     sleep_speed = 0
-    ''' Uncomment the following line for feedback while the program is running '''
-    #print "Going really fast..."
     counter = 100
+
     while counter > 0:
-        ''' Uncomment the following line for feedback while the program is running '''
-        #print "counter = ", counter
+        LOGGER.debug("counter = %s", counter)
         roy_g_biv(sleep_speed)
         # decrease counter
         counter -= 1
     sleep(2)
-    increase_speed()
 
-#Start the program
-try:
-    increase_speed()
-# Stop the program and turn off LEDs with Ctrl-C
-except KeyboardInterrupt:
-    pyglow.all()
+
+def delete_empty_logs():
+    """
+    Delete empty log fles
+
+    Log files will always be created. But they will be empty if the
+    log level is set to anything higher than DEBUG, since only DEBUG
+    messages are logged. If the log files are empty, they will be
+    deleted.
+    """
+
+    logs = [LOG, 'print_piglow_header.log']
+
+    for log in logs:
+        if os.stat(log).st_size == 0:
+            os.remove(log)
+
+
+def stop():
+    """
+    Print exit message and turn off the PiGlow
+    """
+    LOGGER.debug("END")
+    delete_empty_logs()
+    print("\nExiting program.")
+    PYGLOW.all(0)
+
+
+if __name__ == '__main__':
+    main()
