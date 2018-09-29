64674a958ac9b5f39ec638f7b38def35f06ad8bb
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2018-09-29 01:31:19 -0500

    Update inside_out_2.py
    Converted to Python 3

--- Programs/inside_out_2.py
+++ Programs/inside_out_2.py
@@ -5,7 +5,7 @@ Inside Out 2
 A variation of Inside Out. This program lights up the
 indivdual colors like in Inside Out, but they stay on until
 all leds are on. Then they are turned off in reverse order.
-Then the speed gradually increases. 
+Then the speed gradually increases.
 
 ....................
 
@@ -60,6 +60,9 @@ from print_piglow_header import print_piglow_header
 PYGLOW = PyGlow()
 PYGLOW.all(0)
 
+# Feel free to modify the brightness setting below
+LED_BRIGHTNESS = 100
+
 # Logging
 LOG = 'inside_out_2.log'
 LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: %(levelname)s: %(message)s'
@@ -105,18 +108,16 @@ def red_leds_on(sleep_speed):
         sleep_speed: the time to wait between LEDs
     """
 
-    # Feel free to modify the brightness setting below
-    led_brightness = 100
     sleep_speed = sleep_speed
 
     # Arm 1, Red
-    PYGLOW.led(13, led_brightness)
+    PYGLOW.led(13, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 2, Red
-    PYGLOW.led(7, led_brightness)
+    PYGLOW.led(7, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 3, Red
-    PYGLOW.led(1, led_brightness)
+    PYGLOW.led(1, LED_BRIGHTNESS)
     sleep(sleep_speed)
 
 
@@ -149,18 +150,16 @@ def orange_leds_on(sleep_speed):
         sleep_speed: the time to wait between LEDs
     """
 
-    # Feel free to modify the brightness setting below
-    led_brightness = 100
     sleep_speed = sleep_speed
 
     # Arm 1, Orange
-    PYGLOW.led(14, led_brightness)
+    PYGLOW.led(14, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 2, Orange
-    PYGLOW.led(8, led_brightness)
+    PYGLOW.led(8, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 3, Orange
-    PYGLOW.led(2, led_brightness)
+    PYGLOW.led(2, LED_BRIGHTNESS)
     sleep(sleep_speed)
 
 
@@ -193,18 +192,16 @@ def yellow_leds_on(sleep_speed):
         sleep_speed: the time to wait between LEDs
     """
 
-    # Feel free to modify the brightness setting below
-    led_brightness = 100
     sleep_speed = sleep_speed
 
     # Arm 1, Yellow
-    PYGLOW.led(15, led_brightness)
+    PYGLOW.led(15, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 2, Yellow
-    PYGLOW.led(9, led_brightness)
+    PYGLOW.led(9, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 3, Yellow
-    PYGLOW.led(3, led_brightness)
+    PYGLOW.led(3, LED_BRIGHTNESS)
     sleep(sleep_speed)
 
 
@@ -237,18 +234,16 @@ def green_leds_on(sleep_speed):
         sleep_speed: the time to wait between LEDs
     """
 
-    # Feel free to modify the brightness setting below
-    led_brightness = 100
     sleep_speed = sleep_speed
 
     # Arm 1, Green
-    PYGLOW.led(16, led_brightness)
+    PYGLOW.led(16, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 2, Green
-    PYGLOW.led(10, led_brightness)
+    PYGLOW.led(10, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 3, Green
-    PYGLOW.led(4, led_brightness)
+    PYGLOW.led(4, LED_BRIGHTNESS)
     sleep(sleep_speed)
 
 
@@ -281,18 +276,16 @@ def blue_leds_on(sleep_speed):
         sleep_speed: the time to wait between LEDs
     """
 
-    # Feel free to modify the brightness setting below
-    led_brightness = 100
     sleep_speed = sleep_speed
 
     # Arm 1, Blue
-    PYGLOW.led(17, led_brightness)
+    PYGLOW.led(17, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 2, Blue
-    PYGLOW.led(11, led_brightness)
+    PYGLOW.led(11, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 3, Blue
-    PYGLOW.led(5, led_brightness)
+    PYGLOW.led(5, LED_BRIGHTNESS)
     sleep(sleep_speed)
 
 
@@ -325,18 +318,16 @@ def white_leds_on(sleep_speed):
         sleep_speed: the time to wait between LEDs
     """
 
-    # Feel free to modify the brightness setting below
-    led_brightness = 100
     sleep_speed = sleep_speed
 
     # Arm 1, White
-    PYGLOW.led(18, led_brightness)
+    PYGLOW.led(18, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 2, White
-    PYGLOW.led(12, led_brightness)
+    PYGLOW.led(12, LED_BRIGHTNESS)
     sleep(sleep_speed)
     # Arm 3, White
-    PYGLOW.led(6, led_brightness)
+    PYGLOW.led(6, LED_BRIGHTNESS)
     sleep(sleep_speed)
 
 
@@ -427,11 +418,11 @@ def go_faster():
     LOGGER.debug("Going faster...")
 
     sleep_speed = 0.01
-    counter = 20
 
-    while counter > 0:
+    # Start counter at 1, end at 20, increment by 1
+    for i in range(1, 21, 1):
         # Uncomment the following line for testing/debugging
-        LOGGER.debug("counter = %s", counter)
+        LOGGER.debug("counter = %s", i)
         # Turn on LEDs
         white_leds_on(sleep_speed)
         blue_leds_on(sleep_speed)
@@ -446,8 +437,6 @@ def go_faster():
         green_leds_off(sleep_speed)
         blue_leds_off(sleep_speed)
         white_leds_off(sleep_speed)
-        # decrease counter
-        counter -= 1
 
 
 def go_really_fast():
@@ -457,10 +446,10 @@ def go_really_fast():
     LOGGER.debug("Going really fast...")
 
     sleep_speed = 0
-    counter = 100
 
-    while counter > 0:
-        LOGGER.debug("counter = %s", counter)
+    # Start counter at 1, end at 100, increment by 1
+    for i in range(1, 101, 1):
+        LOGGER.debug("counter = %s", i)
         # Turn on LEDs
         white_leds_on(sleep_speed)
         blue_leds_on(sleep_speed)
@@ -475,9 +464,6 @@ def go_really_fast():
         green_leds_off(sleep_speed)
         blue_leds_off(sleep_speed)
         white_leds_off(sleep_speed)
-        # decrease counter
-        # decrease counter
-        counter -= 1
 
 
 def delete_empty_logs():
