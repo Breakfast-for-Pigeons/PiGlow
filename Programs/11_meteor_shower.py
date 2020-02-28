89dff8c6d5e671286c4a199fa7541ff5ab665329
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-28 16:48:33 -0600

    Update and rename meteor_shower.py to 11_meteor_shower.py

--- Programs/11_meteor_shower.py
+++ Programs/11_meteor_shower.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Meteor Shower
 
@@ -15,14 +15,12 @@ Functions:
                    then fades them.
 - shooting_star_3: lights up the LEDs on arm 3 one at a time and
                    then fades them.
-- delete_empty_logs: Deletes empty log fles
-- stop: Print exit message and turn off the PiGlow
 
 ....................
 
 Requirements:
     PyGlow.py (many thanks to benleb for this program)
-    print_piglow_header.py
+    bfp_piglow_modules.py
 
 You will have these files if you downloaded the entire repository.
 
@@ -36,11 +34,13 @@ This program was written on a Raspberry Pi using the Geany IDE.
 #                          Import modules                              #
 ########################################################################
 
-import os
 import logging
 from time import sleep
 from PyGlow import PyGlow
-from print_piglow_header import print_piglow_header
+from bfp_piglow_modules import print_header
+from bfp_piglow_modules import check_log_directory
+from bfp_piglow_modules import delete_empty_logs
+from bfp_piglow_modules import stop
 
 ########################################################################
 #                           Initialize                                 #
@@ -49,59 +49,9 @@ from print_piglow_header import print_piglow_header
 PYGLOW = PyGlow()
 PYGLOW.all(0)
 
-# Logging
-LOG = 'meteor_shower.log'
-LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: %(levelname)s: %(message)s'
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.ERROR)    # Nothing will log unless changed to DEBUG
-FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
-                              datefmt='%m/%d/%y %I:%M:%S %p:')
-FILE_HANDLER = logging.FileHandler(LOG, 'w')
-FILE_HANDLER.setFormatter(FORMATTER)
-LOGGER.addHandler(FILE_HANDLER)
-
 ########################################################################
 #                            Functions                                 #
 ########################################################################
-
-
-def main():
-    """ The main fuction """
-    LOGGER.debug("START")
-
-    print_piglow_header()
-
-    # Force white text after selecting random colored header
-    print("\033[1;37;40mPress Ctrl-C to stop the program.")
-    try:
-        # 1, 2, and 3
-        shooting_star_1()
-        shooting_star_2()
-        shooting_star_3()
-        # 2, 3, 1
-        shooting_star_2()
-        shooting_star_3()
-        shooting_star_1()
-        # 3, 1, 2
-        shooting_star_3()
-        shooting_star_1()
-        shooting_star_2()
-        # 1, 3, 2
-        shooting_star_1()
-        shooting_star_3()
-        shooting_star_2()
-        # 3, 2, 1
-        shooting_star_3()
-        shooting_star_2()
-        shooting_star_1()
-        # 2, 1, 3
-        shooting_star_2()
-        shooting_star_1()
-        shooting_star_3()
-        stop()
-    # Stop the program and turn off LEDs with Ctrl-C
-    except KeyboardInterrupt:
-        stop()
 
 
 def shooting_star_1():
@@ -216,7 +166,7 @@ def shooting_star_1():
 
 
 def shooting_star_2():
-    ''' Turn on Arm 2 LEDS and fade '''
+    """ Turn on Arm 2 LEDS and fade """
     LOGGER.debug("Shooting Star 2")
 
     sleep_speed = 0.01
@@ -327,7 +277,7 @@ def shooting_star_2():
 
 
 def shooting_star_3():
-    ''' Turn on Arm 3 LEDS and fade '''
+    """ Turn on Arm 3 LEDS and fade """
     LOGGER.debug("Shooting Star 3")
 
     sleep_speed = 0.01
@@ -437,32 +387,64 @@ def shooting_star_3():
     sleep(0.25)
 
 
-def delete_empty_logs():
+def main():
     """
-    Delete empty log fles
-
-    Log files will always be created. But they will be empty if the
-    log level is set to anything higher than DEBUG, since only DEBUG
-    messages are logged. If the log files are empty, they will be
-    deleted.
+    The main function.
     """
-
-    logs = [LOG, 'print_piglow_header.log']
-
-    for log in logs:
-        if os.stat(log).st_size == 0:
-            os.remove(log)
-
-
-def stop():
-    """
-    Print exit message and turn off the PiGlow
-    """
+    LOGGER.debug("START")
+    
+    # 1, 2, and 3
+    shooting_star_1()
+    shooting_star_2()
+    shooting_star_3()
+    # 2, 3, 1
+    shooting_star_2()
+    shooting_star_3()
+    shooting_star_1()
+    # 3, 1, 2
+    shooting_star_3()
+    shooting_star_1()
+    shooting_star_2()
+    # 1, 3, 2
+    shooting_star_1()
+    shooting_star_3()
+    shooting_star_2()
+    # 3, 2, 1
+    shooting_star_3()
+    shooting_star_2()
+    shooting_star_1()
+    # 2, 1, 3
+    shooting_star_2()
+    shooting_star_1()
+    shooting_star_3()
     LOGGER.debug("END")
-    delete_empty_logs()
-    print("\nExiting program.")
-    PYGLOW.all(0)
+    
+    delete_empty_logs(LOG)
+    stop()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        # STEP01: Check if Log directory exits.
+        check_log_directory()
+        # STEP02: Enable logging
+        LOG = 'Logs/11_meteor_shower.log'
+        LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
+                      %(levelname)s: %(message)s'
+        LOGGER = logging.getLogger(__name__)
+        # Nothing will log unless logging level is changed to DEBUG
+        LOGGER.setLevel(logging.ERROR)
+        FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
+                                      datefmt='%m/%d/%y %I:%M:%S %p:')
+        FILE_HANDLER = logging.FileHandler(LOG, 'w')
+        FILE_HANDLER.setFormatter(FORMATTER)
+        LOGGER.addHandler(FILE_HANDLER)
+        # STEP03: Print header
+        print_header()
+        # STEP04: Print instructions in white text
+        print("\033[1;37;40mPress Ctrl-C to stop the program.")
+        # STEP05: Run the main function
+        main()
+    except KeyboardInterrupt:
+        delete_empty_logs(LOG)
+        stop()
