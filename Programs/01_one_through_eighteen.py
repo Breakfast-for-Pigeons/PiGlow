766279ae5d4f7ef7bbe13a35cc32a88eb76902ac
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-28 15:16:02 -0600

    Update 01_one_through_eighteen.py

--- Programs/01_one_through_eighteen.py
+++ Programs/01_one_through_eighteen.py
@@ -30,9 +30,12 @@ This program was written on a Raspberry Pi using the Geany IDE.
 #                          Import modules                              #
 ########################################################################
 
+import logging
 from time import sleep
 from PyGlow import PyGlow
 from bfp_piglow_modules import print_header
+from bfp_piglow_modules import check_log_directory
+from bfp_piglow_modules import delete_empty_logs
 from bfp_piglow_modules import stop
 
 ########################################################################
@@ -53,11 +56,13 @@ def one_through_eighteen_1():
     and ending with LED 18. The process is done a total of 5 times, each
     time increasing the speed by 0.05.
     """
+    LOGGER.debug("Function: one_through_eighteen_1")
+    LOGGER.debug("Lighting up LEDs 1 through 18...")
 
     sleep_speed = 0.25
 
     while sleep_speed > 0.05:
-
+        LOGGER.debug("The speed is now %s", sleep_speed)
         # Start led number at 1, end at 18, increment by 1
         for i in range(1, 19, 1):
             PYGLOW.led(i, 100)       # light up LED
@@ -72,11 +77,13 @@ def one_through_eighteen_2():
     Lights up and turns off one LED one at a time, starting with LED 1
     and ending with LED 18. Cycles through all the LEDs 10 times at a
     speed of 0.05.
-
     """
+    LOGGER.debug("Function: one_through_eighteen_2")
+    LOGGER.debug("Running 10 times...")
 
-    # Start counter at 1, end at 10
-    for _ in range(1, 10):
+    # Start counter at 1, end at 10, increment by 1
+    for i in range(1, 11, 1):
+        LOGGER.debug("counter is: %s", i)
         # Start led number at 1, end at 18, increment by 1
         for j in range(1, 19, 1):
             PYGLOW.led(j, 100)    # light up LED
@@ -85,16 +92,40 @@ def one_through_eighteen_2():
             sleep(0.05)
 
 
+def main():
+    """
+    This is the main function.
+    """
+    LOGGER.debug("START")
+    one_through_eighteen_1()
+    one_through_eighteen_2()
+    LOGGER.debug("END")
+    delete_empty_logs(LOG)
+    stop()
+
+
 if __name__ == '__main__':
     try:
-        # STEP01: Print header
+        # STEP01: Check if Log directory exits.
+        check_log_directory()
+        # STEP02: Enable logging
+        LOG = 'Logs/01_one_through_eighteen.log'
+        LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
+                      %(levelname)s: %(message)s'
+        LOGGER = logging.getLogger(__name__)
+        # Nothing will log unless logging level is changed to DEBUG
+        LOGGER.setLevel(logging.ERROR)
+        FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
+                                      datefmt='%m/%d/%y %I:%M:%S %p:')
+        FILE_HANDLER = logging.FileHandler(LOG, 'w')
+        FILE_HANDLER.setFormatter(FORMATTER)
+        LOGGER.addHandler(FILE_HANDLER)
+        # STEP03: Print header
         print_header()
-        # STEP02: Print instructions in white text
+        # STEP04: Print instructions in white text
         print("\033[1;37;40mPress Ctrl-C to stop the program.")
-        # STEP03: Light up LEDs 1 through 18
-        one_through_eighteen_1()
-        one_through_eighteen_2()
-        # STEP04: Exit the program.
-        stop()
+        # STEP05: Run the main function
+        main()
     except KeyboardInterrupt:
+        delete_empty_logs(LOG)
         stop()
