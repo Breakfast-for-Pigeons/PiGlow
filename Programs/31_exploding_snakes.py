9d442d37005d87b87aae9b563567d92c3c4b263f
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 00:53:03 -0600

    Update and rename exploding_snakes.py to 31_exploding_snakes.py

--- Programs/31_exploding_snakes.py
+++ Programs/31_exploding_snakes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Exploding Snakes
 
@@ -12,14 +12,12 @@ Functions:
 - explode_snake_12_or_21: Lights up and turns off the LEDs on arms 1 and 2
 - explode_snake_13_or_31: Lights up and turns off the LEDs on arms 1 and 3
 - explode_snake_23_or_32: Lights up and turns off the LEDs on arms 2 and 3
-- delete_empty_logs: Deletes empty log fles
-- stop: Print exit message and turn off the PiGlow
 
 ....................
 
 Requirements:
     PyGlow.py (many thanks to benleb for this program)
-    print_piglow_header.py
+    bfp_piglow_modules.py
 
 You will have these files if you downloaded the entire repository.
 
@@ -33,11 +31,13 @@ This program was written on a Raspberry Pi using the Geany IDE.
 #                          Import modules                              #
 ########################################################################
 
-import os
 import logging
 from time import sleep
 from PyGlow import PyGlow
-from print_piglow_header import print_piglow_header
+from bfp_piglow_modules import print_header
+from bfp_piglow_modules import check_log_directory
+from bfp_piglow_modules import delete_empty_logs
+from bfp_piglow_modules import stop
 
 ########################################################################
 #                           Initialize                                 #
@@ -46,61 +46,9 @@ from print_piglow_header import print_piglow_header
 PYGLOW = PyGlow()
 PYGLOW.all(0)
 
-# Logging
-LOG = 'exploding_snakes.log'
-LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: %(levelname)s: %(message)s'
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.ERROR)    # Nothing will log unless changed to DEBUG
-FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
-                              datefmt='%m/%d/%y %I:%M:%S %p:')
-FILE_HANDLER = logging.FileHandler(LOG, 'w')
-FILE_HANDLER.setFormatter(FORMATTER)
-LOGGER.addHandler(FILE_HANDLER)
-
 ########################################################################
 #                            Functions                                 #
 ########################################################################
-
-
-def main():
-    """
-    The main function
-    """
-    LOGGER.debug("START")
-
-    print_piglow_header()
-
-    # Force white text after selecting random colored header
-    print("\033[1;37;40mPress Ctrl-C to stop the program.")
-    try:
-        # 12, 13, 23
-        exploding_snake_12()
-        exploding_snake_13()
-        exploding_snake_23()
-        # 13, 23, 12
-        exploding_snake_13()
-        exploding_snake_23()
-        exploding_snake_12()
-        # 23, 12, 13
-        exploding_snake_23()
-        exploding_snake_12()
-        exploding_snake_13()
-        # 12, 23, 13
-        exploding_snake_12()
-        exploding_snake_23()
-        exploding_snake_13()
-        # 23, 13, 12
-        exploding_snake_23()
-        exploding_snake_13()
-        exploding_snake_12()
-        # 13, 12, 23
-        exploding_snake_13()
-        exploding_snake_12()
-        exploding_snake_23()
-        stop()
-    # Stop the program and turn off LEDs with Ctrl-C
-    except KeyboardInterrupt:
-        stop()
 
 
 def exploding_snake_12():
@@ -118,8 +66,11 @@ def exploding_snake_12():
         LOGGER.debug("Pulse speed is: %s", i)
         PYGLOW.led(snake_12_leds, 100, speed=i, pulse=True)
         sleep(0)
+
+    # Pause 1 second before exploding
     PYGLOW.led(snake_12_leds, 100)
     sleep(1)
+
     # Explode Snake 12
     LOGGER.debug("Exploding Snake 12...")
     PYGLOW.led(6, 0)
@@ -165,8 +116,11 @@ def exploding_snake_13():
         LOGGER.debug("Pulse speed is: %s", i)
         PYGLOW.led(snake_13_leds, 100, speed=i, pulse=True)
         sleep(0)
+
+    # Pause 1 second before exploding
     PYGLOW.led(snake_13_leds, 100)
     sleep(1)
+
     # Explode Snake 13
     LOGGER.debug("Exploding Snake 13...")
     PYGLOW.led(18, 0)
@@ -212,8 +166,11 @@ def exploding_snake_23():
         LOGGER.debug("Pulse speed is: %s", i)
         PYGLOW.led(snake_23_leds, 100, speed=i, pulse=True)
         sleep(0)
+
+    # Pause 1 second before exploding
     PYGLOW.led(snake_23_leds, 100)
     sleep(1)
+
     # Explode Snake 13
     LOGGER.debug("Exploding Snake 23...")
     PYGLOW.led(6, 0)
@@ -244,32 +201,66 @@ def exploding_snake_23():
     sleep(2)
 
 
-def delete_empty_logs():
+def main():
     """
-    Delete empty log fles
-
-    Log files will always be created. But they will be empty if the
-    log level is set to anything higher than DEBUG, since only DEBUG
-    messages are logged. If the log files are empty, they will be
-    deleted.
+    The main function
     """
+    LOGGER.debug("START")
 
-    logs = [LOG, 'print_piglow_header.log']
+    # 12, 13, 23
+    exploding_snake_12()
+    exploding_snake_13()
+    exploding_snake_23()
+    # 13, 23, 12
+    exploding_snake_13()
+    exploding_snake_23()
+    exploding_snake_12()
+    # 23, 12, 13
+    exploding_snake_23()
+    exploding_snake_12()
+    exploding_snake_13()
+    # 12, 23, 13
+    exploding_snake_12()
+    exploding_snake_23()
+    exploding_snake_13()
+    # 23, 13, 12
+    exploding_snake_23()
+    exploding_snake_13()
+    exploding_snake_12()
+    # 13, 12, 23
+    exploding_snake_13()
+    exploding_snake_12()
+    exploding_snake_23()
 
-    for log in logs:
-        if os.stat(log).st_size == 0:
-            os.remove(log)
+    LOGGER.debug("START")
 
+    delete_empty_logs(LOG)
+    stop()
 
-def stop():
-    """
-    Print exit message and turn off the PiGlow
-    """
-    LOGGER.debug("END")
-    delete_empty_logs()
-    print("\nExiting program.")
-    PYGLOW.all(0)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        # STEP01: Check if Log directory exists.
+        check_log_directory()
+        # STEP02: Enable logging
+        LOG = 'Logs/31_exploding_snakes.log'
+        LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
+                      %(levelname)s: %(message)s'
+        LOGGER = logging.getLogger(__name__)
+        # Nothing will log unless logging level is changed to DEBUG
+        LOGGER.setLevel(logging.ERROR)
+        FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
+                                      datefmt='%m/%d/%y %I:%M:%S %p:')
+        FILE_HANDLER = logging.FileHandler(LOG, 'w')
+        FILE_HANDLER.setFormatter(FORMATTER)
+        LOGGER.addHandler(FILE_HANDLER)
+        # STEP03: Print header
+        print_header()
+        # STEP04: Print instructions in white text
+        print("\033[1;37;40mPress Ctrl-C to stop the program.")
+        # STEP05: Run the main function
+        main()
+    except KeyboardInterrupt:
+        delete_empty_logs(LOG)
+        stop()
