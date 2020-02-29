9c49efeebb7b0da1e5fa97aa12b72d87d515fef7
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-28 19:54:37 -0600

    Update snakes.py

--- Programs/snakes.py
+++ Programs/snakes.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Snakes (or should the title be: Pythons)
+Snakes (or should the title be: Pythons ?)
 
 I think the title is self explanatory.
 
@@ -10,14 +10,12 @@ Functions:
 - snake_12: Lights up the LEDs on arms 1 and 2
 - snake_13: Lights up the LEDs on arms 1 and 3
 - snake_23: Lights up the LEDs on arms 2 and 3
-- delete_empty_logs: Deletes empty log fles
-- stop: Print exit message and turn off the PiGlow
 
 ....................
 
 Requirements:
     PyGlow.py (many thanks to benleb for this program)
-    print_piglow_header.py
+    bfp_piglow_modules.py
 
 You will have these files if you downloaded the entire repository.
 
@@ -31,11 +29,13 @@ This program was written on a Raspberry Pi using the Geany IDE.
 #                          Import modules                              #
 ########################################################################
 
-import os
 import logging
 from time import sleep
 from PyGlow import PyGlow
-from print_piglow_header import print_piglow_header
+from bfp_piglow_modules import print_header
+from bfp_piglow_modules import check_log_directory
+from bfp_piglow_modules import delete_empty_logs
+from bfp_piglow_modules import stop
 
 ########################################################################
 #                           Initialize                                 #
@@ -44,44 +44,10 @@ from print_piglow_header import print_piglow_header
 PYGLOW = PyGlow()
 PYGLOW.all(0)
 
-# Logging
-LOG = 'snakes.log'
-LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: %(levelname)s: %(message)s'
-LOGGER = logging.getLogger(__name__)
-LOGGER.setLevel(logging.ERROR)    # Nothing will log unless changed to DEBUG
-FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
-                              datefmt='%m/%d/%y %I:%M:%S %p:')
-FILE_HANDLER = logging.FileHandler(LOG, 'w')
-FILE_HANDLER.setFormatter(FORMATTER)
-LOGGER.addHandler(FILE_HANDLER)
-
 ########################################################################
 #                            Functions                                 #
 ########################################################################
 
-
-def main():
-    """
-    The main function
-    """
-    LOGGER.debug("START")
-
-    print_piglow_header()
-
-    # Force white text after selecting random colored header
-    print("\033[1;37;40mPress Ctrl-C to stop the program.")
-
-    try:
-        # Start counter at 1, end at 3, increment by 1
-        for i in range(1, 4, 1):
-            LOGGER.debug("counter = %s", i)
-            snake_12()
-            snake_23()
-            snake_13()
-        stop()
-    # Stop the program and turn off LEDs with Ctrl-C
-    except KeyboardInterrupt:
-        stop()
 
 def snake_12():
     """
@@ -89,7 +55,7 @@ def snake_12():
     """
     LOGGER.debug("Lighting up Snake 1-2")
 
-    # Snake 12 LEDs (Same lights as Snake 21 - Since they are not
+    # Snake 12 LEDs (Same lights as Snake 21 - Because they are not
     # slithering, the order doesn't matter)
     snake_12_leds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 18]
     sleep_speed = 2
@@ -108,7 +74,7 @@ def snake_13():
     """
     LOGGER.debug("Lighting up Snake 1-3")
 
-    # Snake 13 LEDs (Same lights as Snake 31 - Since they are not
+    # Snake 13 LEDs (Same lights as Snake 31 - Because they are not
     # slithering, the order doesn't matter)
     snake_13_leds = [1, 2, 3, 4, 5, 12, 13, 14, 15, 16, 17, 18]
     sleep_speed = 2
@@ -127,7 +93,7 @@ def snake_23():
     """
     LOGGER.debug("Lighting up Snake 2-3")
 
-    # Snake 23 LEDs (Same lights as Snake 32 - Since they are not
+    # Snake 23 LEDs (Same lights as Snake 32 - Because they are not
     # slithering, the order doesn't matter)
     snake_23_leds = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
     sleep_speed = 2
@@ -140,32 +106,47 @@ def snake_23():
     sleep(1)
 
 
-def delete_empty_logs():
+def main():
     """
-    Delete empty log fles
-
-    Log files will always be created. But they will be empty if the
-    log level is set to anything higher than DEBUG, since only DEBUG
-    messages are logged. If the log files are empty, they will be
-    deleted.
+    The main function
     """
+    LOGGER.debug("START")
 
-    logs = [LOG, 'print_piglow_header.log']
+    # Start counter at 1, end at 3, increment by 1
+    for i in range(1, 4, 1):
+        LOGGER.debug("counter = %s", i)
+        snake_12()
+        snake_23()
+        snake_13()
 
-    for log in logs:
-        if os.stat(log).st_size == 0:
-            os.remove(log)
-
-
-def stop():
-    """
-    Print exit message and turn off the PiGlow
-    """
     LOGGER.debug("END")
-    delete_empty_logs()
-    print("\nExiting program.")
-    PYGLOW.all(0)
+
+    delete_empty_logs(LOG)
+    stop()
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        # STEP01: Check if Log directory exists.
+        check_log_directory()
+        # STEP02: Enable logging
+        LOG = 'Logs/20_snakes.log'
+        LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
+                      %(levelname)s: %(message)s'
+        LOGGER = logging.getLogger(__name__)
+        # Nothing will log unless logging level is changed to DEBUG
+        LOGGER.setLevel(logging.ERROR)
+        FORMATTER = logging.Formatter(fmt=LOG_FORMAT,
+                                      datefmt='%m/%d/%y %I:%M:%S %p:')
+        FILE_HANDLER = logging.FileHandler(LOG, 'w')
+        FILE_HANDLER.setFormatter(FORMATTER)
+        LOGGER.addHandler(FILE_HANDLER)
+        # STEP03: Print header
+        print_header()
+        # STEP04: Print instructions in white text
+        print("\033[1;37;40mPress Ctrl-C to stop the program.")
+        # STEP05: Run the main function
+        main()
+    except KeyboardInterrupt:
+        delete_empty_logs(LOG)
+        stop()
