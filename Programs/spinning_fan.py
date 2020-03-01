05ba1fb163679cd48b2c01053627adc0884af68d
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:29:37 -0600

    Update and rename 29_spinning_fan.py to spinning_fan.py

--- Programs/spinning_fan.py
+++ Programs/spinning_fan.py
@@ -177,7 +177,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exists.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/29_spinning_fan.log'
+        LOG = 'Logs/spinning_fan.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
