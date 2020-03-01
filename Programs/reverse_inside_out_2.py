304920163bf71134ec29ba47bbc1f4d38fdc974c
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:24:23 -0600

    Update and rename 16_reverse_inside_out_2.py to reverse_inside_out_2.py

--- Programs/reverse_inside_out_2.py
+++ Programs/reverse_inside_out_2.py
@@ -422,7 +422,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exists.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/16_reverse_inside_out_2.log'
+        LOG = 'Logs/reverse_inside_out_2.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
