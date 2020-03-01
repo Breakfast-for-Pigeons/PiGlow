bfcacb5cc4337fe6fcd939090193ba417cdddf30
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:27:29 -0600

    Update and rename 25_flux_capacitor.py to flux_capacitor.py

--- Programs/flux_capacitor.py
+++ Programs/flux_capacitor.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exists.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/25_flux_capacitor.log'
+        LOG = 'Logs/flux_capacitor.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
