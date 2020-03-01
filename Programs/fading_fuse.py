7b671100fcf74fa8e9017588220b5d8a6e8a2732
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:34:56 -0600

    Update and rename 36_fading_fuse.py to fading_fuse.py

--- Programs/fading_fuse.py
+++ Programs/fading_fuse.py
@@ -1391,7 +1391,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exists.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/36_fading_fuse.log'
+        LOG = 'Logs/fading_fuse.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
