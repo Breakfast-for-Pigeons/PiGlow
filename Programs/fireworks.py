2c8bae6aa919454329c0327dc8b1d1bf819d7a1e
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:25:08 -0600

    Update and rename 19_fireworks.py to fireworks.py

--- Programs/fireworks.py
+++ Programs/fireworks.py
@@ -178,7 +178,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exists.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/19_firworks.log'
+        LOG = 'Logs/firworks.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
