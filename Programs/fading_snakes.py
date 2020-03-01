008bd7a3ece9c0cf301dedce70c94296776335d5
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:25:55 -0600

    Update and rename 21_fading_snakes.py to fading_snakes.py

--- Programs/fading_snakes.py
+++ Programs/fading_snakes.py
@@ -208,7 +208,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exists.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/21_fading_snakes.log'
+        LOG = 'Logs/fading_snakes.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
