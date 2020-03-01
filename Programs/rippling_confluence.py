640b3c8449fb95294868b220c2d622e7d90dffdc
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:36:01 -0600

    Update and rename 39_rippling_confluence.py to rippling_confluence.py

--- Programs/rippling_confluence.py
+++ Programs/rippling_confluence.py
@@ -675,7 +675,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exists.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/39_rippling_confluence.log'
+        LOG = 'Logs/rippling_confluence.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
