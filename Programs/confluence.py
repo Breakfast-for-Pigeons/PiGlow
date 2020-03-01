0886fda45ed47a53231c0a3631fc4b852116ab82
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:35:40 -0600

    Update and rename 38_confluence.py to confluence.py

--- Programs/confluence.py
+++ Programs/confluence.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exists.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/38_confluence.log'
+        LOG = 'Logs/confluence.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
