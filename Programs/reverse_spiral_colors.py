4b854bf9e0a42b04e080d0d8e6eb393bb78a391c
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:19:32 -0600

    Update and rename 08_reverse_spiral_colors.py to reverse_spiral_colors.py

--- Programs/reverse_spiral_colors.py
+++ Programs/reverse_spiral_colors.py
@@ -312,7 +312,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exits.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/08_reverse_spiral_colors.log'
+        LOG = 'Logs/reverse_spiral_colors.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
