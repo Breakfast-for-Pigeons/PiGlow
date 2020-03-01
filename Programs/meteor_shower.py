762e2ad3eb3f182d5921f0ad888e778df907850f
Paul <13591438+Breakfast-for-Pigeons@users.noreply.github.com>
2020-02-29 19:21:04 -0600

    Update and rename 11_meteor_shower.py to meteor_shower.py

--- Programs/meteor_shower.py
+++ Programs/meteor_shower.py
@@ -428,7 +428,7 @@ if __name__ == '__main__':
         # STEP01: Check if Log directory exits.
         check_log_directory()
         # STEP02: Enable logging
-        LOG = 'Logs/11_meteor_shower.log'
+        LOG = 'Logs/meteor_shower.log'
         LOG_FORMAT = '%(asctime)s %(name)s: %(funcName)s: \
                       %(levelname)s: %(message)s'
         LOGGER = logging.getLogger(__name__)
